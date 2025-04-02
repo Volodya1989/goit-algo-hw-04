74e49897cd62e99ec6bc9b7ebf4d83092f118fa7
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2025-04-02 06:30:18 -0400

    init commit

---
+++ task_4_1.py
@@ -0,0 +1,5 @@
+
+# expected outcome
+# total, average = total_salary("path/to/salary_file.txt")
+# print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+# Загальна сума заробітної плати: 6000, Середня заробітна плата: 2000

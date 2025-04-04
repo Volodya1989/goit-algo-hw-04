3226ced174248ddcbd9d0c869f7149989f963fd2
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2025-04-03 23:57:11 -0400

    replaced elif with match

--- task_4_4.py
+++ task_4_4.py
@@ -48,21 +48,23 @@ def main():
         user_input = input("Enter a command: ")
         command, *args = parse_input(user_input)
 
-        if command in ["close", "exit"]:
-            print("Good bye!")
-            break
-        elif command == "hello":
-            print("How can I help you?")
-        elif command == "add":
-            print(add_contact(args, contacts))
-        elif command == "change":
-            print(change_contact(args, contacts))
-        elif command == "phone":
-            print(show_phone(args, contacts))
-        elif command == "all":
-            print(show_phone(contacts))
-        else:
-            print("Invalid command.")
+        match command:
+            case "close" | "exit":
+                print("Good bye!")
+                break
+            case "hello":
+               print("How can I help you?")
+            case "add":
+               print(add_contact(args, contacts))
+            case "change":
+               print(change_contact(args, contacts))
+            case "phone":
+              print(show_phone(args, contacts))
+            case "all":
+                print(show_phone(contacts))
+            case _:
+                print("Invalid command. Please try again.")
+
 
 
 if __name__ == "__main__":
